7a5bb4e58b8f9ea8746cee89e0a46c2e4f636421
hsw0905 <hsw0905@gmail.com>
2021-05-17 23:44:05 +0900

    leetcode: longest_substring_without_repeating_characters init

---
+++ daily-ps/20210517/test_longest_substring_without_repeating_characters.py
@@ -0,0 +1,10 @@
+def solution(s: str) -> int:
+    answer = 0
+    return answer
+
+
+def test_solution():
+    assert solution(s='abcabcbb') == 3
+    assert solution(s='bbbbb') == 1
+    assert solution(s='') == 0
+    assert solution(s='pwwkew') == 3
