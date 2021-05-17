71c2db3ea86fd00924f2a9784780bdf6511ca51b
hsw0905 <hsw0905@gmail.com>
2021-05-18 00:37:14 +0900

    leetcode: longest_substring_without_repeating_characters solved

--- daily-ps/20210517/test_longest_substring_without_repeating_characters.py
+++ daily-ps/20210517/test_longest_substring_without_repeating_characters.py
@@ -1,6 +1,18 @@
 def solution(s: str) -> int:
-    answer = 0
-    return answer
+    length = len(s)
+    if length < 2:
+        return length
+    hash_map = {}
+
+    left = -1
+    longest = 1
+
+    for right in range(length):
+        if s[right] in hash_map:
+            left = max(hash_map[s[right]], left)
+        hash_map[s[right]] = right
+        longest = max(right-left, longest)
+    return longest
 
 
 def test_solution():
