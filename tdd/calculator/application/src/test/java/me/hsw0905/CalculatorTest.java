c4277b26db43282966d9f57c803f80b6b0eb8f61
hsw0905 <hsw0905@gmail.com>
2021-09-26 01:24:30 +0900

    feat: add simple test code

--- tdd/calculator/application/src/test/java/me/hsw0905/CalculatorTest.java
+++ tdd/calculator/application/src/test/java/me/hsw0905/CalculatorTest.java
@@ -1,14 +1,35 @@
 package me.hsw0905;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
-    @Test
-    void create() {
-        Calculator calculator = new Calculator();
-        assertNotNull(calculator);
+    private static Calculator cal;
+
+    @BeforeAll
+    static void setUp() {
+        cal = new Calculator();
     }
 
+    @Test
+    void add() {
+        assertEquals(9, cal.add(6, 3));
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(5, cal.subtract(9, 4));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(10, cal.multiply(2, 5));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2, cal.divide(4, 2));
+    }
 }
